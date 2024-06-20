66448031247a6b49cdeebf31a45f6a9eb652c27b
wxm <2023342552@qq.com>
2024-06-20 10:40:16 +0800

    使用国内镜像的gradle插件仓库

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 > 参考：[阿里云原生脚手架](https://start.aliyun.com/)
 
-目标：
+**目标：**
 
 1. 使用 Spring Boot 2.x 版本（这里是 2.3.12.RELEASE）
 2. 多模块项目
@@ -10,7 +10,11 @@
 4. 使用 Kotlin Gradle 脚本构建
 5. 使用 Java 1.8 编译源码
 
-实现：
+**实现：**
 
 1. 使用 Gradle 6.x 构建（这里是 6.9.3，5.x 版本应该也可以）
 2. 使用 Java 11 编译 Gradle 脚本
+
+**特点：**
+
+gradle wrapper、gradle plugin、依赖仓库都使用国内镜像的地址。
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,15 @@
+buildscript {
+    repositories {
+        maven { setUrl("https://maven.aliyun.com/repository/gradle-plugin") }
+        gradlePluginPortal()
+    }
+}
+
 plugins {
-    id("org.springframework.boot") version "2.3.12.RELEASE"
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    id("org.jetbrains.kotlin.jvm") version "1.3.72"
-    id("org.jetbrains.kotlin.plugin.spring") version "1.3.72"
+    id("org.springframework.boot") version "2.3.12.RELEASE" apply false
+    id("io.spring.dependency-management") version "1.0.11.RELEASE" apply false
+    id("org.jetbrains.kotlin.jvm") version "1.3.72" apply false
+    id("org.jetbrains.kotlin.plugin.spring") version "1.3.72" apply false
 }
 
 allprojects {
